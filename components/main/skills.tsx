a72354231e8135f5a4cf986e39958d5525726f77
urvish <kaneriyaurvish@gmail.com>
2025-06-24 00:07:58 +0530

    Remove unused FULLSTACK_SKILL and OTHER_SKILL components from Skills section to streamline code.

--- components/main/skills.tsx
+++ components/main/skills.tsx
@@ -4,8 +4,6 @@ import { SkillText } from "@/components/sub/skill-text";
 import {
   BACKEND_SKILL,
   FRONTEND_SKILL,
-  FULLSTACK_SKILL,
-  OTHER_SKILL,
   SKILL_DATA,
 } from "@/constants";
 
@@ -45,30 +43,6 @@ export const Skills = () => {
       </div>
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
         {BACKEND_SKILL.map((skill, i) => (
-          <SkillDataProvider
-            key={skill.skill_name}
-            src={skill.image}
-            name={skill.skill_name}
-            width={skill.width}
-            height={skill.height}
-            index={i}
-          />
-        ))}
-      </div>
-      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {FULLSTACK_SKILL.map((skill, i) => (
-          <SkillDataProvider
-            key={skill.skill_name}
-            src={skill.image}
-            name={skill.skill_name}
-            width={skill.width}
-            height={skill.height}
-            index={i}
-          />
-        ))}
-      </div>
-      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {OTHER_SKILL.map((skill, i) => (
           <SkillDataProvider
             key={skill.skill_name}
             src={skill.image}
